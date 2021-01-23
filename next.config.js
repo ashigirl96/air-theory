fa81df05f45ed0c0316fd300108ecbafda73a2cd
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-01-24 00:39:19 +0900

    not use baseUrl, but use basePath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const path = require('path');
 
 module.exports = {
-  baseUrl: process.env.GITHUB_PAGES ? "/air-theory" : "",
+  basePath: process.env.GITHUB_PAGES ? "/air-theory" : "",
   assetPrefix: process.env.GITHUB_PAGES ? "/air-theory" : "",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
