7057c7939b8dae8fe9fffa29c891a9a10109448d
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-01-24 00:04:59 +0900

    add workflow

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const path = require('path');
 
 module.exports = {
-  baseUrl: "/air-theory",
-  assetPrefix: "/air-theory",
+  baseUrl: process.env.GITHUB_PAGES ? "/air-theory" : "",
+  assetPrefix: process.env.GITHUB_PAGES ? "/air-theory" : "",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
